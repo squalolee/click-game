69781ff2fa54e6343856a6c9545302833eac1998
Solomon <squalolee@gmail.com>
2020-09-22 19:55:45 -0400

    Set up structure for code

---
+++ src/components/Title/title.js
@@ -0,0 +1,8 @@
+import { findAllByTitle } from "@testing-library/react";
+import React from "react";
+
+function Title(props) {
+    
+}
+
+export default Title;
---
+++ src/components/Wrapper/wrapper.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Wrapper(props) {
+
+}
+export default Wrapper;
