92e79ef1ea169c00eeeea073a246aca310775129
Solomon <squalolee@gmail.com>
2020-09-22 20:00:28 -0400

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
